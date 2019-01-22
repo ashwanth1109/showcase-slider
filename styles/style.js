ef2f547c5ab329b9cd2d3c2877fc51392093cf49
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-22 23:34:25 +0530

    Common styles

---
+++ styles/style.js
@@ -0,0 +1,65 @@
+export const fCenter = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+};
+
+export const center = {
+    justifyContent: "center",
+    alignItems: "center"
+};
+
+export const fScreen = {
+    width: "100vw",
+    height: "100vh"
+};
+
+export const w = {
+    w100p: {
+        width: "100%"
+    },
+    w1000: {
+        width: "1000px"
+    }
+};
+
+export const h = {
+    h200: {
+        height: "200px"
+    }
+};
+
+export const m = {
+    hAuto: {
+        margin: "0 auto"
+    }
+};
+
+export const flex = {
+    row: {
+        display: "flex",
+        flexDirection: "row"
+    },
+    col: {
+        display: "flex",
+        flexDirection: "column"
+    }
+};
+
+export const pos = {
+    justifyEvenly: {
+        justifyContent: "space-evenly"
+    }
+};
+
+export const bg = {
+    black: {
+        backgroundColor: "#222"
+    },
+    white: {
+        backgroundColor: "#fff"
+    },
+    transparent: {
+        backgroundColor: "transparent"
+    }
+};
