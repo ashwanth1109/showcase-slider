db783db7aad32e98c9eb7c84083ddb24f7094c73
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-22 23:34:48 +0530

    Custom Next server

---
+++ server.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import next from "next";
+import express from "express";
+
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT;
+const rootUrl = process.env.ROOT_URL;
+
+const app = next({ dev });
+const handler = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+        const server = express();
+        server.use(express.json());
+
+        server.get("/unsplash", (req, res) => {
+            res.status(200).json({ test: "Route works" });
+        });
+
+        server.get("*", (req, res) => handler(req, res));
+
+        server.listen(port, err => {
+            if (err) throw err;
+            console.log(`> Ready on ${rootUrl}`);
+        });
+    })
+    .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    });
