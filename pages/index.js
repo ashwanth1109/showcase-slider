825d924360d0439dafe748072e068b0b48c62603
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-22 23:35:19 +0530

    Index Page fetches public data from unsplash and renders ProjectShowcase

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,34 @@
 // import dependencies
 // ------------------------------------------------------------
 import withRedux from "../HOC/withRedux";
+import { index as s } from "../styles/page";
+import ProjectShowcase from "../components/ProjectShowcase";
+import { extractURL } from "../utility";
 // ------------------------------------------------------------
 // index Component
 // ------------------------------------------------------------
-const Index = () => {
-    return (
-        <div>
-            <div>Index</div>
-        </div>
-    );
-};
+class Index extends React.Component {
+    async componentDidMount() {
+        const res = await fetch("https://api.unsplash.com/photos/", {
+            method: "GET",
+            headers: {
+                Authorization: `Client-ID ${process.env.ACCESS_KEY}`
+            }
+        });
+        const data = await res.json();
+        const urls = extractURL(data);
+        console.log(urls);
+    }
+    render() {
+        console.log(this.state);
+        return (
+            <div style={s.container}>
+                <ProjectShowcase />
+                <ProjectShowcase />
+            </div>
+        );
+    }
+}
 // ------------------------------------------------------------
 // export index
 // ------------------------------------------------------------
