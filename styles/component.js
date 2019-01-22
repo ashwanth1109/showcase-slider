29b677a19c94c75b37db2c0ff53f79b59b81b020
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-22 23:34:39 +0530

    Component Styling

---
+++ styles/component.js
@@ -0,0 +1,15 @@
+import { bg } from "./style";
+
+// ------------------------------------------------------------
+// project showcase component
+// ------------------------------------------------------------
+export const projectShowcase = {
+    container: {
+        width: "400px",
+        height: "200px",
+        ...bg.white,
+        borderRadius: "5px",
+        position: "relative",
+        zIndex: "-1"
+    }
+};
