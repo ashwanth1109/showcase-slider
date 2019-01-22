c5268135e318c8f6caa1b5ea08eabf6d1d5df624
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-22 23:34:32 +0530

    Page styles

---
+++ styles/page.js
@@ -0,0 +1,13 @@
+import { bg, w, m, flex, pos, h } from "./style";
+
+// ------------------------------------------------------------
+// index
+// ------------------------------------------------------------
+export const index = {
+    container: {
+        ...w.w1000,
+        ...m.hAuto,
+        ...flex.row,
+        ...pos.justifyEvenly
+    }
+};
