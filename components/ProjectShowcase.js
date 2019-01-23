ee0819616b4d63ce73faeef23fc42d15926d7369
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-23 19:07:06 +0530

    ProjectShowcase is customizable to prop inputs

--- components/ProjectShowcase.js
+++ components/ProjectShowcase.js
@@ -1,16 +1,15 @@
 import { projectShowcase as s } from "../styles/component";
 import withRedux from "../HOC/withRedux";
+import { extractURL } from "../utility";
 
 class ProjectShowcase extends React.Component {
     state = {
         first: {
-            w: 400,
             t: true,
             id: 0,
             position: 0
         },
         second: {
-            w: 0,
             t: true,
             id: 1,
             position: 400
@@ -20,26 +19,50 @@ class ProjectShowcase extends React.Component {
             complete: 0,
             incomplete: 400,
             transition: true,
-            completeColor: "#ffffff80",
-            incompleteColor: "#00000080"
+            completeColor: this.props.completeColor
         }
     };
 
-    componentDidMount({ progress } = this.state) {
-        setTimeout(() => {
-            progress.complete = 400;
-            progress.incomplete = 0;
-            this.setState({
-                progress
-            });
-        }, 2000);
-        this.timer = setInterval(() => {
-            this.step1(
-                this.state.first,
-                this.state.second,
-                this.state.progress
-            );
-        }, 4000);
+    async componentDidMount(
+        { progress } = this.state,
+        {
+            updateState,
+            actionType,
+            query,
+            items,
+            stayTime,
+            transitionTime
+        } = this.props
+    ) {
+        try {
+            const queryParams = [
+                `client_id=${process.env.ACCESS_KEY}`,
+                `query=${query}`
+            ];
+            const unsplashURL =
+                "https://api.unsplash.com/search/photos/?" +
+                queryParams.join("&");
+            const res = await fetch(unsplashURL);
+            const data = await res.json();
+            const imagesData = extractURL(data.results, items);
+            updateState(actionType, imagesData);
+            setTimeout(() => {
+                progress.complete = 400;
+                progress.incomplete = 0;
+                this.setState({
+                    progress
+                });
+            }, transitionTime * 1000);
+            this.timer = setInterval(() => {
+                this.step1(
+                    this.state.first,
+                    this.state.second,
+                    this.state.progress
+                );
+            }, (stayTime + transitionTime) * 1000);
+        } catch (err) {
+            console.error(err);
+        }
     }
 
     componentWillUnmount() {
@@ -49,10 +72,10 @@ class ProjectShowcase extends React.Component {
     // ------------------------------------------------------------
     // first moves left, second moves left simultaneously
     // ------------------------------------------------------------
-    step1 = (first, second, progress) => {
+    step1 = (first, second, progress, { transitionTime } = this.props) => {
         first.position = -400;
         second.position = 0;
-        progress.completeColor = "#00000080";
+        progress.completeColor = this.props.incompleteColor;
         this.setState({
             first,
             second,
@@ -63,18 +86,18 @@ class ProjectShowcase extends React.Component {
             progress.transition = false;
             progress.incomplete = 400;
             progress.complete = 0;
-            progress.completeColor = "#ffffff80";
+            progress.completeColor = this.props.completeColor;
             this.setState({
                 progress
             });
+            this.step2(first, second, progress);
             setTimeout(() => {
                 progress.transition = true;
                 progress.incomplete = 0;
                 progress.complete = 400;
                 this.setState({ progress });
-                this.step2(first, second);
             }, 100);
-        }, 1900);
+        }, transitionTime * 1000);
     };
     // ------------------------------------------------------------
     // turn off transition
@@ -118,7 +141,7 @@ class ProjectShowcase extends React.Component {
     };
 
     render(
-        { images } = this.props,
+        { images, transitionTime, stayTime } = this.props,
         { first, second, dotId, progress } = this.state
     ) {
         if (images.length > 0) {
@@ -128,7 +151,9 @@ class ProjectShowcase extends React.Component {
                         style={{
                             ...s.imgContainer,
                             ...s.firstContainer,
-                            transition: first.t ? "2s ease-in-out left" : "",
+                            transition: first.t
+                                ? `${transitionTime}s ease-in-out left`
+                                : "",
                             left: first.position
                         }}
                     >
@@ -142,7 +167,9 @@ class ProjectShowcase extends React.Component {
                         style={{
                             ...s.imgContainer,
                             ...s.secondContainer,
-                            transition: second.t ? "2s ease-in-out left" : "",
+                            transition: second.t
+                                ? `${transitionTime}s ease-in-out left`
+                                : "",
                             left: second.position
                         }}
                     >
@@ -161,6 +188,7 @@ class ProjectShowcase extends React.Component {
                                         <div
                                             style={{
                                                 ...s.dot,
+                                                transition: `${transitionTime}s ease-in-out background-color`,
                                                 ...(dotId === id
                                                     ? s.selected
                                                     : s.unselected)
@@ -176,17 +204,17 @@ class ProjectShowcase extends React.Component {
                                         backgroundColor: progress.completeColor,
                                         width: progress.complete,
                                         transition: progress.transition
-                                            ? "2s linear width, 2s ease-in-out background-color"
+                                            ? `${stayTime}s linear width, ${transitionTime}s ease-in-out background-color`
                                             : ""
                                     }}
                                 />
                                 <div
                                     style={{
-                                        backgroundColor:
-                                            progress.incompleteColor,
+                                        backgroundColor: this.props
+                                            .incompleteColor,
                                         width: progress.incomplete,
                                         transition: progress.transition
-                                            ? "2s linear width, 2s ease-in-out background-color"
+                                            ? `${stayTime}s linear width, ${transitionTime}s ease-in-out background-color`
                                             : ""
                                     }}
                                 />
@@ -205,4 +233,4 @@ class ProjectShowcase extends React.Component {
     }
 }
 
-export default withRedux(ProjectShowcase, true, false);
+export default withRedux(ProjectShowcase, true, true);
