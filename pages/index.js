d4152e84f2a82aa2b87c58ddcbd500cb6c669f84
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-23 19:06:33 +0530

    Moved api calls to component and setting custom props to ProjectShowcase

--- pages/index.js
+++ pages/index.js
@@ -4,28 +4,33 @@
 import withRedux from "../HOC/withRedux";
 import { index as s } from "../styles/page";
 import ProjectShowcase from "../components/ProjectShowcase";
-import { extractURL } from "../utility";
 // ------------------------------------------------------------
 // index Component
 // ------------------------------------------------------------
 class Index extends React.Component {
-    async componentDidMount({ updateState } = this.props) {
-        const res = await fetch("https://api.unsplash.com/photos/", {
-            method: "GET",
-            headers: {
-                Authorization: `Client-ID ${process.env.ACCESS_KEY}`
-            }
-        });
-        const data = await res.json();
-        const urlArrays = extractURL(data);
-        updateState("IMAGES1", urlArrays[0]);
-        updateState("IMAGES2", urlArrays[1]);
-    }
-    render({ images1, images2 } = this.props) {
+    render() {
         return (
             <div style={s.container}>
-                <ProjectShowcase images={images1} />
-                <ProjectShowcase images={images2} />
+                <ProjectShowcase
+                    images={this.props.images1} // images array state from mapState
+                    actionType="IMAGES1" // redux action dispatcher type to store state
+                    query="office" // query term for images
+                    items={3} // should be between 2 - 10
+                    completeColor="#ffffff80" // color to indicate complete in progress bar
+                    incompleteColor="#000000AA" // color to indicate incomplete in progress bar
+                    stayTime={2} // time component waits on each image
+                    transitionTime={2} // time component takes to transition slides
+                />
+                <ProjectShowcase
+                    images={this.props.images2}
+                    actionType="IMAGES2"
+                    query="space"
+                    items={5}
+                    completeColor="#ffffff80"
+                    incompleteColor="#000000AA"
+                    stayTime={3}
+                    transitionTime={1}
+                />
             </div>
         );
     }
