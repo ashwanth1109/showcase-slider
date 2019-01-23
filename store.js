0d16b568fd51dd38ea0889c41ae2a637377b5985
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-24 02:36:58 +0530

    Added reducers for images3 and images4

--- store.js
+++ store.js
@@ -5,8 +5,8 @@ import { combineReducers } from "redux";
 // ------------------------------------------------------------
 // "TEST" reducer
 // ------------------------------------------------------------
-const test = (state = "Redux works", action) =>
-    action.type === "TEST" ? action.payload : state;
+// const test = (state = "Redux works", action) =>
+//     action.type === "TEST" ? action.payload : state;
 // ------------------------------------------------------------
 // "IMAGES1" reducer - for storing image urls of 1st showcase component
 // ------------------------------------------------------------
@@ -18,10 +18,21 @@ const images1 = (state = [], action) =>
 const images2 = (state = [], action) =>
     action.type === "IMAGES2" ? action.payload : state;
 // ------------------------------------------------------------
+// "IMAGES3" reducer - for storing image urls of 3rd showcase component
+// ------------------------------------------------------------
+const images3 = (state = [], action) =>
+    action.type === "IMAGES3" ? action.payload : state;
+// ------------------------------------------------------------
+// "IMAGES4" reducer - for storing image urls of 4th showcase component
+// ------------------------------------------------------------
+const images4 = (state = [], action) =>
+    action.type === "IMAGES4" ? action.payload : state;
+// ------------------------------------------------------------
 // combineReducers & export
 // ------------------------------------------------------------
 export default combineReducers({
-    test,
     images1,
-    images2
+    images2,
+    images3,
+    images4
 });
