15dc1d613fd26b57e213d95949e20b87d7e574dc
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-24 01:59:39 +0530

    Added unsplashURL construct function

--- utility.js
+++ utility.js
@@ -1,3 +1,6 @@
+// ------------------------------------------------------------
+// utility function to extract urls and username from unsplash API response
+// ------------------------------------------------------------
 export const extractURL = (arr, numOfItems, urls = []) => {
     // console.log(arr);
     for (const item of arr) {
@@ -8,4 +11,17 @@ export const extractURL = (arr, numOfItems, urls = []) => {
         urls.push(image);
     }
     return urls.slice(0, numOfItems);
+};
+// ------------------------------------------------------------
+// utility function to get unsplash url to search for photos
+// with client id and query parameter
+// ------------------------------------------------------------
+export const getUnsplashURL = query => {
+    // query params
+    const queryParams = [
+        `client_id=${process.env.ACCESS_KEY}`,
+        `query=${query}`
+    ];
+    // return constructed url
+    return "https://api.unsplash.com/search/photos/?" + queryParams.join("&");
 };
