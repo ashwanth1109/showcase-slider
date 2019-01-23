8329b2e60e7436a51c2f5135b971e190ad3f4042
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-24 01:59:50 +0530

    Added comments

--- styles/style.js
+++ styles/style.js
@@ -1,24 +1,35 @@
+// ------------------------------------------------------------
+// center across both directions in a flex container
+// ------------------------------------------------------------
 export const fCenter = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
 };
-
+// ------------------------------------------------------------
+// same as fCenter, but in case display flex already applied
+// ------------------------------------------------------------
 export const center = {
     justifyContent: "center",
     alignItems: "center"
 };
-
+// ------------------------------------------------------------
+// in case a page / component should occupy full screen size
+// ------------------------------------------------------------
 export const fScreen = {
     width: "100vw",
     height: "100vh"
 };
-
+// ------------------------------------------------------------
+// in case a component should occupy full size of parent
+// ------------------------------------------------------------
 export const fSize = {
     width: "100%",
     height: "100%"
 };
-
+// ------------------------------------------------------------
+// common styles for width
+// ------------------------------------------------------------
 export const w = {
     w100p: {
         width: "100%"
@@ -27,7 +38,9 @@ export const w = {
         maxWidth: "1000px"
     }
 };
-
+// ------------------------------------------------------------
+// common styles for height
+// ------------------------------------------------------------
 export const h = {
     h200: {
         height: "200px"
@@ -36,13 +49,17 @@ export const h = {
         height: "100%"
     }
 };
-
+// ------------------------------------------------------------
+// common styles for margin
+// ------------------------------------------------------------
 export const m = {
     hAuto: {
         margin: "0 auto"
     }
 };
-
+// ------------------------------------------------------------
+// row and column display in flex
+// ------------------------------------------------------------
 export const flex = {
     row: {
         display: "flex",
@@ -53,7 +70,9 @@ export const flex = {
         flexDirection: "column"
     }
 };
-
+// ------------------------------------------------------------
+// positioning an element using justify or align
+// ------------------------------------------------------------
 export const pos = {
     justifyEvenly: {
         justifyContent: "space-evenly"
@@ -65,7 +84,9 @@ export const pos = {
         justifyContent: "space-between"
     }
 };
-
+// ------------------------------------------------------------
+// background color styles
+// ------------------------------------------------------------
 export const bg = {
     black: {
         backgroundColor: "#222"
