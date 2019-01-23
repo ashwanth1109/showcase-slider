fd6460c326eb1b9bee9216e3eaab732f336d3449
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-23 16:10:57 +0530

    Added component styles for project showcase

--- styles/component.js
+++ styles/component.js
@@ -1,4 +1,4 @@
-import { bg } from "./style";
+import { bg, flex, fCenter, fSize, pos, h, m } from "./style";
 
 // ------------------------------------------------------------
 // project showcase component
@@ -10,6 +10,78 @@ export const projectShowcase = {
         ...bg.white,
         borderRadius: "5px",
         position: "relative",
-        zIndex: "-1"
+        overflow: "hidden",
+        ...flex.row
+    },
+    imgContainer: {
+        width: "400px",
+        height: "200px",
+        maxHeight: "200px",
+        position: "absolute"
+    },
+    image: {
+        position: "absolute",
+        width: "400px",
+        height: "200px",
+        objectFit: "cover"
+    },
+    user: {
+        position: "absolute",
+        color: "#fff",
+        bottom: "5px",
+        left: "25px",
+        zIndex: "1"
+    },
+    firstContainer: {
+        left: "0px"
+    },
+    firstImage: {
+        right: "0px"
+    },
+    secondContainer: {
+        right: "0px"
+    },
+    secondImage: {
+        left: "0px"
+    },
+    infoContainer: {
+        width: "100%",
+        height: "30px",
+        position: "absolute",
+        bottom: "0px",
+        padding: "0px 30px",
+        boxSizing: "border-box"
+    },
+    info: {
+        ...fSize,
+        ...flex.row
+    },
+    dots: {
+        ...h.h100p,
+        ...flex.row,
+        ...pos.alignCenter,
+        zIndex: "1"
+    },
+    dot: {
+        width: "10px",
+        height: "10px",
+        borderRadius: "6px",
+        border: "1px solid #fff",
+        margin: "0 3.5px",
+        transition: "2s ease-in-out background-color"
+    },
+    selected: {
+        backgroundColor: "#fff"
+    },
+    unselected: {
+        backgroundColor: "#ffffff00"
+    },
+    progress: {
+        ...flex.row,
+        position: "absolute",
+        width: "100%",
+        height: "30px",
+        bottom: "0px",
+        left: "0px"
     }
 };
