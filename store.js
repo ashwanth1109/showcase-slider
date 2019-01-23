879677ba0c21714627d7bc68160951ccffb332ee
Ashwanth A R <ashwanth1109@gmail.com>
2019-01-24 02:00:29 +0530

    Added comments for images reducer

--- store.js
+++ store.js
@@ -7,13 +7,16 @@ import { combineReducers } from "redux";
 // ------------------------------------------------------------
 const test = (state = "Redux works", action) =>
     action.type === "TEST" ? action.payload : state;
-
+// ------------------------------------------------------------
+// "IMAGES1" reducer - for storing image urls of 1st showcase component
+// ------------------------------------------------------------
 const images1 = (state = [], action) =>
     action.type === "IMAGES1" ? action.payload : state;
-
+// ------------------------------------------------------------
+// "IMAGES2" reducer - for storing image urls of 2nd showcase component
+// ------------------------------------------------------------
 const images2 = (state = [], action) =>
     action.type === "IMAGES2" ? action.payload : state;
-
 // ------------------------------------------------------------
 // combineReducers & export
 // ------------------------------------------------------------
